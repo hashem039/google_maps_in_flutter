53a1138177757b4e735b90f0b12738dedaafa05f
Hashem <hm.engineer039@gmail.com>
2022-02-06 17:18:32 +0400

    using await and async to load bitmap image as marker icon add custom marker icon add custom marker same way we can add Polygon, polyline, circle. also we can add map style using json object.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'src/locations.dart' as locations;
@@ -15,6 +18,37 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final Map<String, Marker> _markers = {};
+
+  //hm1 note: same way we can add polygons and polyline or circle.
+  Set<Marker> _customMarkers = HashSet<Marker>();
+  late GoogleMapController _mapController;
+  //hm1 adding custom icon for the marker
+  //bitmap image that represent the icon
+  late BitmapDescriptor  _markerIcon;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _setMarkerIcon();
+  }
+  void _setMarkerIcon() async {
+    _markerIcon = await BitmapDescriptor.fromAssetImage(ImageConfiguration(), "assets/hashem.ico");
+
+  }
+
+  void _onMapCreated1(GoogleMapController controller) {
+    _mapController = controller;
+
+    setState(() {
+      _customMarkers.add(new Marker(
+        markerId: MarkerId("0"),
+        position: LatLng(25.2048, 55.2708),
+        infoWindow: InfoWindow(title: "Dubai", snippet: "lovelly Dubai"),
+        icon: _markerIcon,));
+    });
+  }
+
   Future<void> _onMapCreated(GoogleMapController controller) async {
     final googleOffices = await locations.getGoogleOffices();
     setState(() {
@@ -27,6 +61,7 @@ class _MyAppState extends State<MyApp> {
             title: office.name,
             snippet: office.address,
           ),
+
         );
         _markers[office.name] = marker;
       }
@@ -44,16 +79,16 @@ class _MyAppState extends State<MyApp> {
         body: Stack(
           children: [
             GoogleMap(
-              onMapCreated: _onMapCreated,
+              onMapCreated: _onMapCreated1,
               initialCameraPosition: const CameraPosition(
-                target: LatLng(0, 0),
+                target: LatLng(25.2048, 55.2708),
                 zoom: 2,
               ),
-              markers: _markers.values.toSet(),
+              markers: _customMarkers//_markers.values.toSet(),
             ),
             Container(alignment: Alignment.bottomCenter,
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 32.0),
-            child: Text('This is a test widget'),)
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 32.0),
+              child: Text('This is a test widget'),)
           ],
         ),
       ),
